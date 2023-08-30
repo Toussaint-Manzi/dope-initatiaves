8e1ddab9d9870375936f57a7537133a73e695a5a
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-08-30 21:32:51 +0200

    styling second section of the page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,15 @@ import styles from '../styles/home.module.css'
 import Image from 'next/image'
 import logo from '../../public/assets/images/logo.png'
 import screens from '../../public/assets/images/screens.png'
+import logo2 from '../../public/assets/images/logo2.png'
+import playStore from '../../public/assets/images/playstore.png'
+import appStore from '../../public/assets/images/appstore.png'
+
 
 
 const Home = () => {
   return (
-    <div className={`${styles.myFont}flex flex-col h-screen bg-white px-72`}>
+    <div className={`${styles.myFont}flex flex-col h-screen bg-white px-44`}>
         <div className='flex justify-end'>
             <div className=' w-32 h-24 relative '>
                 <Image
@@ -16,8 +20,8 @@ const Home = () => {
                 />
             </div>
         </div>
-        <div className='w-full'>
-            <div className='w-2/3 flex items-start relative'>
+        <div className='w-full flex justify-between items-center'>
+            <div className='w-[60%] flex self-start relative'>
                 <div className='h-[34rem] w-[50%] absolute rounded-3xl my-auto bg-[#ff6896] z-0'>
 
                 </div>
@@ -30,6 +34,30 @@ const Home = () => {
                     </div>
                 </div>
                
+            </div>
+            <div className='w-[35%] h-[34rem] flex flex-col justify-center'>
+                <div className='h-[70px] w-[90px] relative'>
+                    <Image
+                        src={logo2}
+                        fill
+                    />
+                </div>
+                <h1 className={`${styles.myFont2} uppercase text-lg text-[#ff6896] mt-10`}>keza quiz</h1>
+                <p className={`${styles.myFont} text-md text-black`} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia optio adipisci obcaecati voluptate? Sed maiores officiis nisi consequatur harum optio.</p>
+                <div className='flex w-2/3 justify-between mt-10'>
+                    <div className='h-[40px] w-[130px] relative'>
+                        <Image
+                            src={appStore}
+                            fill
+                        />
+                    </div>
+                    <div className='h-[40px] w-[130px] relative'>
+                        <Image
+                            src={playStore}
+                            fill
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
