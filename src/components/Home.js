7f15b9974d59b3f2dca184e4f3dda2cdbff12127
Toussaint MANZI <toussaintmanzi006@gmail.com>
2023-08-31 10:41:23 +0200

    adding responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ import { AiFillInstagram } from 'react-icons/ai'
 
 const Home = () => {
   return (
-    <div className={`${styles.myFont}flex flex-col h-screen bg-white px-44`}>
+    <div className={`${styles.myFont}flex flex-col h-screen bg-white px-44 xl:px-20`}>
         <div className='flex justify-end'>
             <div className=' w-32 h-24 relative '>
                 <Image
@@ -24,11 +24,11 @@ const Home = () => {
             </div>
         </div>
         <div className='w-full flex justify-between items-center'>
-            <div className='w-[60%] flex self-start relative'>
-                <div className='h-[34rem] w-[50%] absolute rounded-3xl my-auto bg-[#ff6896] z-0'>
+            <div className='w-[60%] flex self-start relative xl:w-[70%]'>
+                <div className='h-[34rem] w-[50%] absolute rounded-3xl my-auto bg-[#ff6896] z-0 larg:w-[60%]'>
 
                 </div>
-                <div className='h-[28rem] my-10 w-[60%] absolute z-10 ml-32 flex flex-col'>
+                <div className='h-[28rem] my-10 w-[60%] absolute z-10 ml-32 flex flex-col larg:w-[70%] larg:ml-24'>
                     <div className='h-full w-full relative'>
                         <Image
                             src={screens}
@@ -65,13 +65,13 @@ const Home = () => {
                 <h1 className={`${styles.myFont2} uppercase text-lg text-[#ff6896] mt-10`}>keza quiz</h1>
                 <p className={`${styles.myFont} text-md text-black`} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia optio adipisci obcaecati voluptate? Sed maiores officiis nisi consequatur harum optio.</p>
                 <div className='flex w-2/3 justify-between mt-10'>
-                    <div className='h-[40px] w-[130px] relative cursor-pointer'>
+                    <div className='h-[45px] w-[130px] relative cursor-pointer'>
                         <Image
                             src={appStore}
                             fill
                         />
                     </div>
-                    <div className='h-[40px] w-[130px] relative cursor-pointer'>
+                    <div className='h-[45px] w-[130px] relative cursor-pointer'>
                         <Image
                             src={playStore}
                             fill
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xsm': {'min': '480px'},    
+        'msm': {'min': '700px'},
+        'lg': {'max': '1025px'},
+        'larg': {'max': '950px'},
+        'md': {'max': '767px'},
+        'sm': {'max': '700px'},
+        'xtra': {'max': '400px'},    
+        'lgm': {'min': '1025px'},
+        '2xl': {'max': '1535px'},
+        'xs': {'max': '500px'},
+        'xl': {'max': '1279px'},
+
+      },
       fontFamily: {
         'sans': ['Outfit', 'sans-serif'],
       },
