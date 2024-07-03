8e7a8a16e73b4ec0bf2b8120835049c8845bcbf8
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-03 14:16:17 +0200

    added social media changes responsivenss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -107,17 +107,17 @@ const Footer = () => {
         </div>
         <div className='mt-10 w-full flex justify-between items-center xl:flex-col'>
             <p className='text-gray-500'>Dope initiatives © {new Date().getFullYear()} All rights reserved</p>
-            <div className='flex justify-between w-[20%] xl:w-[35%] my-5 xs:w-[50%]'>
-                <Link href="https://www.instagram.com/dope_initiatives/" target='_blank' className='w-[45px] h-[45px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
+            <div className='flex justify-between w-[20%] xl:w-[35%] my-5 xs:w-[60%]'>
+                <Link href="https://www.instagram.com/dope_initiatives/" target='_blank' className='w-[45px] h-[45px] xs:w-[40px] xs:h-[40px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
                     <AiFillInstagram className='text-white text-xl'/>
                 </Link>
-                <Link href="https://x.com/Dope_Initiative" target='_blank' className='w-[45px] h-[45px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
-                    <BsTwitter className='text-white text-lg'/>
+                <Link href="https://x.com/Dope_Initiative" target='_blank' className='w-[45px] h-[45px] xs:w-[40px] xs:h-[40px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 256 256"><path fill="#fff" d="m218.12 209.56l-61-95.8l59.72-65.69a12 12 0 0 0-17.76-16.14l-55.27 60.84l-37.69-59.21A12 12 0 0 0 96 28H48a12 12 0 0 0-10.12 18.44l61 95.8l-59.76 65.69a12 12 0 1 0 17.76 16.14l55.31-60.84l37.69 59.21A12 12 0 0 0 160 228h48a12 12 0 0 0 10.12-18.44M166.59 204L69.86 52h19.55l96.73 152Z"/></svg>
                 </Link>
-                <Link href="https://www.linkedin.com/company/dopeinitiatives" target='_blank' className='w-[45px] h-[45px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
+                <Link href="https://www.linkedin.com/company/dopeinitiatives" target='_blank' className='w-[45px] h-[45px] xs:w-[40px] xs:h-[40px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
                     <BsLinkedin className='text-white text-lg'/>
                 </Link>
-                <Link href="mailto:dopeinitiativesltd@gmail.com" target='_blank' className='w-[45px] h-[45px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
+                <Link href="mailto:dopeinitiativesltd@gmail.com" target='_blank' className='w-[45px] h-[45px] xs:w-[40px] xs:h-[40px] bg-orange-500 rounded-full flex justify-center items-center cursor-pointer hover:bg-amber-500 hover:transition-all hover:duration-300'>
                     <MdEmail className='text-white text-lg'/>
                 </Link>
             </div>
