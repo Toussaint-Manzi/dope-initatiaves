3894eea7449ff05119ea9b98bbef2320ab5c80df
Toussaint-Manzi <toussaintmanzi006@gmail.com>
2024-07-03 13:45:59 +0200

    added some changes on about us and othe packages pages

--- src/pages/keza-eshop.js
+++ src/pages/keza-eshop.js
@@ -4,11 +4,17 @@ import ProductDetails from '@/components/ProductDetails'
 import React from 'react'
 
 const kezaEshop = () => {
+  const detailsArray = [
+    "By providing a centralized marketplace for maternity and reproductive health products, we're addressing a critical need while creating opportunities for local entrepreneurs and businesses to thrive.",
+    "Our carefully curated selection of products includes everything from prenatal vitamins and breastfeeding accessories to menstrual cups and feminine hygiene products. We understand that every woman's journey is unique, which is why we offer a diverse range of options to cater to individual needs and preferences.",
+    "A portion of our proceeds is dedicated to supporting sexual and reproductive health and rights (SRHR) victims, ensuring that they have access to the resources and support they need to rebuild their lives. At Ishyo Shop, we believe in creating a world where every woman can thrive, free from fear, stigma, and barriers to essential healthcare."
+  ];
   return (
     <div>
       <ProductDetails 
         title="Keza E-shop" 
         details="We believe that every woman, deserves access to high-quality maternity and reproductive health products. That's why we're proud to introduce Keza Shop, an innovative e-commerce platform that aims to put an end to the scarcity of these essential items." 
+        paragraphs = { detailsArray }
         coverPhoto="/assets/images/Pose 1.png"
         logo="/assets/images/shop1.png"
       />
--- src/pages/keza-pad.js
+++ src/pages/keza-pad.js
@@ -4,11 +4,19 @@ import ProductDetails from '@/components/ProductDetails'
 import React from 'react'
 
 const kezaPad = () => {
+  const detailsArray = [
+    "Keza Pad Bank vending machines will be strategically placed in high schools across the country, providing a discreet and convenient way for students to obtain sanitary pads when needed. With just a simple tap of a card, girls will be able to access high-quality pads, eliminating the stress and embarrassment often associated with menstruation.",
+    "By subsidizing the cost of pads, we're making sure that financial constraints don't stand in the way of a girl's education and well-being. Every girl, regardless of her socioeconomic background, deserves the freedom to attend school and participate in daily activities without the burden of period stigma.",
+    "At the heart of this initiative lies a powerful message: menstruation is a natural and healthy process, and it should never be a source of shame or disadvantage. By providing easy access to sanitary pads, we're helping to normalize conversations around menstrual health and break down the taboos that have long surrounded this topic.",
+    "Keza Pad Bank is a collaborative effort, and we're calling on schools, organizations, and individuals to join us in this mission. By donating to our cause, you're not only contributing pads but also investing in the future of young women, ensuring that they can pursue their dreams without compromising their dignity or education.",
+    "Together, we can create a world where period poverty is a thing of the past, and every girl has the resources and support she needs to navigate this natural part of life with confidence and ease.",
+  ];
   return (
     <div>
       <ProductDetails 
         title="Keza Pad Bank" 
         details="We believe that every girl deserves to have her basic needs met, including access to essential feminine hygiene products. That's why we're proud to introduce Keza Pad Bank, a novel vending machine initiative that aims to tackle period poverty and ensure that no girl misses school due to lack of access to sanitary pads." 
+        paragraphs={detailsArray}
         coverPhoto="/assets/images/Pose 1.png"
         logo="/assets/images/Logo-2.png"
       />
